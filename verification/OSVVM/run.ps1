afe34428ce3f342f439985ad839c91e22957b202
umarcor <unai.martinezcorral@ehu.eus>
2021-01-20 21:35:18 +0100

    test scripts from 'umarcor/master'

--- verification/OSVVM/run.ps1
+++ verification/OSVVM/run.ps1
@@ -1,4 +1,4 @@
 git submodule update --init --recursive verification\OSVVM\OsvvmLibraries
-& $env:GHDL_PREFIX\vendors\compile-osvvm.ps1 `
+& .\ghdl\scripts\vendors\compile-osvvm.ps1 `
   -All `
   -Source verification\OSVVM\OsvvmLibraries -Output precompiled
--- verification/OSVVM/run.sh
+++ verification/OSVVM/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 git submodule update --init --recursive verification/OSVVM/OsvvmLibraries
-$GHDL_PREFIX/vendors/compile-osvvm.sh \
+./ghdl/scripts/vendors/compile-osvvm.sh \
   --all \
   --source $(dirname $0)/OsvvmLibraries/osvvm --output precompiled
