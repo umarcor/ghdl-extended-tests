3f363bda92b9499d2c754bb7d599dc1fba64a543
umarcor <unai.martinezcorral@ehu.eus>
2021-01-20 21:35:18 +0100

    test scripts from 'umarcor'

--- verification/UVVM/run.ps1
+++ verification/UVVM/run.ps1
@@ -1,4 +1,4 @@
 git submodule update --init --recursive verification\UVVM\uvvm
-& $env:GHDL_PREFIX\vendors\compile-uvvm.ps1 `
+& .\ghdl\scripts\vendors\compile-uvvm.ps1 `
   -All `
   -Source verification\UVVM\uvvm -Output precompiled
--- verification/UVVM/run.sh
+++ verification/UVVM/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 git submodule update --init --recursive verification/UVVM/uvvm
-$GHDL_PREFIX/vendors/compile-uvvm.sh \
+./ghdl/scripts/vendors/compile-uvvm.sh \
   --all \
   --source $(dirname $0)/uvvm --output precompiled
